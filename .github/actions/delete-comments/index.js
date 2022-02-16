c221318d588c590530e2112c66c4ce48343cb314
balakine <balakine@users.noreply.github.com>
2022-02-16 18:27:37 +0200

    Update index.js

--- .github/actions/delete-comments/index.js
+++ .github/actions/delete-comments/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const core = require('@actions/core');
+//const core = require('@actions/core');
 const github = require('@actions/github');
 
+const payload = github.context.payload;
+const user_login = payload.comment.user.login;
+const user_id = payload.comment.user.id;
+const user_type = payload.comment.user.type;
+
+console.log(`User login: ${user_login}, user id: ${user_id}, user type: ${user_type}`);
+if (user_login !== 'balakine' || user_id !== 9373325 || user_type !== 'User')
+    process.exit()
 console.log('New comment');
-const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
